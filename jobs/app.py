8fc1d7c1f923a46f4e9a7c71ec3937110547dcf8
Subbarao Cherukuri <syedava@gmail.com>
2019-10-28 19:15:08 +0530

    Update app.py
    fuxing review

--- jobs/app.py
+++ jobs/app.py
@@ -52,7 +52,7 @@ def employer(employer_id):
     return render_template('employer.html', employer =employer, jobs =jobs, reviews=reviews)
 
 @app.route('/employer/<employer_id>/review', methods =('GET', 'POST'))
-def employer(employer_id):
+def review(employer_id):
     if request.method == 'POST':
         review = request.form['review']
         rating = request.form['rating']
@@ -61,5 +61,5 @@ def employer(employer_id):
         date   = datetime.datetime.now().strftime("%m/%d/%Y")
 
         execute_sql( 'INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',(review, rating, title, date, status, employer_id), commit=True)
-        return redirect(url_for('employer', employer_id=employer_id)) 
+        return redirect(url_for('employer', employer_id=employer_id))
     return render_template('review.html', employer_id=employer_id)
